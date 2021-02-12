a73c4a0519ea97f58b30a49dd9f1d34fa1cf2b69
Merve AKPINAR <56221554+merveakpinar@users.noreply.github.com>
2021-02-12 12:49:41 +0300

    Add files via upload

---
+++ AuditorIntoZombie1_9/Assets/TimeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour
+{
+    [SerializeField] Text timeValue;
+    [SerializeField] float time;
+    private bool gameActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameActive = true;
+        timeValue.text = time.ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameActive==true)
+        { 
+        time -= Time.deltaTime; //iki frame arasındaki gecis suresi icin deltaTime (y2-y1)
+        timeValue.text = ((int)time).ToString();
+        }
+        if (time<0)
+        {
+            time = 60; //dinamik.
+            gameActive = false;
+            GetComponent<PlayerController>().Die();
+
+        }
+    }
+}
